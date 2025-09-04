5d95b2cb9461051476df8f0122f7df37fd5ce4e2
Raul Kolaric <raul.kolaric@gmail.com>
2025-09-04 18:20:17 -0300

    changes

---
+++ src/app/teste/page.tsx
@@ -0,0 +1,14 @@
+import LoginButton from "@/components/LoginButton";
+import Logo from "@/components/Logo";
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Logo></Logo>
+      <LoginButton></LoginButton>
+      <Navbar></Navbar>
+    </div>
+  );
+}
