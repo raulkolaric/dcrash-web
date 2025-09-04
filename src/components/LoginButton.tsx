c86aa8ebc739adcedac180df9e30c1245375b863
Henrique <hahollanda@gmail.com>
2025-09-04 18:02:35 -0300

    Codigo do botao de Login da Navbar

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,18 +1,26 @@
-import { ReactNode } from 'react';
-
 interface LoginButtonProps {
-  variant?: 'primary' | 'secondary';
-  icon?: ReactNode; // ícone opcional
   onClick?: () => void;
+  borderColorClass?: string;
+  backgroundColorClass?: string;
 }
 
-const LoginButton: React.FC<LoginButtonProps> = ({ variant = 'primary', icon }) => {
+export default function LoginButton({
+  onClick,
+  borderColorClass = "border-blue-500",
+  backgroundColorClass = "bg-blue-500",
+}: LoginButtonProps) {
   return (
-    <button className={`btn ${variant}`}>
-      {icon && <span className="icon">{icon}</span>}
+    <button
+      onClick={onClick}
+      className={`
+        border-2 ${borderColorClass} ${backgroundColorClass}
+        text-white font-bold
+        px-4 py-2 rounded-full
+        cursor-pointer
+        transform transition-transform duration-200 hover:scale-110
+      `}
+    >
       Login
     </button>
   );
-};
-
-export default LoginButton;
+}
