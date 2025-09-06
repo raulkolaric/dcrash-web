bdc1674b5a9b7db8d9793acb5c66cc4dae88b53a
Raul Kolaric <raul.kolaric@gmail.com>
2025-09-05 22:46:25 -0300

    color blur background base add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-black">
+    <div className="bg-black w-full min-h-screen flex">
+      <div className="max-w-[1320px] mx-auto justify-center min-h-screen w-full">
+        <div className="bg-[#65D6DE]/20 w-[700px] h-[767px] rounded-full blur-3xl absolute translate-x-1/2 -translate-y-1/2"></div>
+        <p className="text-white">aaaa</p>
+      </div>
     </div>
   );
 }
