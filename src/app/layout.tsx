9648bf17f960dd64e4159e92e7d180433f4beeb1
Henrique <hahollanda@gmail.com>
2025-09-14 13:20:08 -0300

    layout do projeto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,10 @@
-//layout
-
 import "./globals.css";
-import Navbar from "../components/Navbar";
-import Logo from "../components/Logo";
-import LoginButton from "../components/LoginButton";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
       <body className="bg-gray-900 text-white min-h-screen">
-        <header
-          className="flex items-center justify-between px-[25px] bg-black shadow-md mt-[53px]"
-        >
-          {/* Logo alinhada à esquerda */}
-          <Logo width={40} height={40} />
-
-          {/* Navbar central */}
-            <Navbar />
-
-          {/* Botão de login alinhado à direita */}
-          <LoginButton />
-        </header>
+        {children}
       </body>
     </html>
   );
