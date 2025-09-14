c22b31e306cc4fa9774883c19a642952de3af502
Raul Kolaric <raul.kolaric@gmail.com>
2025-09-14 14:53:17 -0300

    fix non-compliant login button - non tailwind style

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -3,16 +3,8 @@
 export default function LoginButton() {
   return (
     <button
-      className="w-[104px] h-[33px] px-4 py-2 rounded-full font-bold"
-      style={{
-        backgroundColor: "#43999F",
-        border: "2px solid #65D6DE",
-        color: "black",
-        boxShadow: "0 4px 6px rgba(101,214,222,0.5)", // sombra do botão (mesma cor da borda)
-        textShadow: "2px 2px 4px rgba(0,0,0,0.3)",
-      }}
-    >
-      Login
+      className="w-[104px] h-[33px] rounded-full font-semibold text-[19px] bg-[#43999F] border-2 border-[rgb(101,214,222)] shadow-[0_0_10px_2px_rgb(101,214,222)]">
+      <p>Login</p>
     </button>
   );
 }
