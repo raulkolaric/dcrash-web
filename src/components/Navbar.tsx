0b6db23c14b7ff873bd33677be10a457d6815e53
Henrique <hahollanda@gmail.com>
2025-09-19 17:45:24 -0300

    Navbar da pagina. Redireciona para as secoes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,12 @@
 
 export default function Navbar({ className = "" }) {
   return (
-    <div className="w-[382px] h-[43px] bg-gradient-to-b from-[#D9D9D933] to-[#73737333] backdrop-blur-[17.1px] rounded-full text-white border-1 border-white flex gap-4 justify-evenly items-center text-[15px] font-thin">
-      <p>Home</p>
-      <p>Missão</p>
-      <p>Futuro</p>
-      <p>Features</p>
+    <div className="w-[382px] h-[43px] bg-gradient-to-b from-[#D9D9D933] to-[#73737333] backdrop-blur-[17.1px] rounded-full text-white border-1 border-white flex gap-4 justify-evenly items-center text-[16px] font-regular">
+      
+      <a href="#secao-home" className="hover:no-underline">Home</a>
+      <a href="#secao-missao" className="hover:no-underline">Missão</a>
+      <a href="#secao-futuro" className="hover:no-underline">Futuro</a>
+      <a href="#secao-features" className="hover:no-underline">Features</a>
       <p>FAQ</p>
     </div>
   );
